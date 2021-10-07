a4e09dc50309913848c5b6dfd0f9ae85251095ae
flaurensia05 <flaurensia05>
2021-10-07 17:54:47 +0700

    MyNavigationDrawer commit

---
+++ MyNavigationDrawer/app/src/main/java/com/example/mynavigationdrawer/ui/subway/SubwayActivity.java
@@ -0,0 +1,16 @@
+package com.example.mynavigationdrawer.ui.subway;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mynavigationdrawer.R;
+
+public class SubwayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_subway);
+    }
+}
